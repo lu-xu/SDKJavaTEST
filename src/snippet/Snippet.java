ceeed4fe1221e3b31f97ddef691d58a2d925bc23
xulu <1510312433@qq.com>
2017-11-13 10:56:28 +0800

    remove all

--- src/snippet/Snippet.java
+++
@@ -1,9 +0,0 @@
-package snippet;
-
-public class Snippet {
-	public static void main(String[] args) {
-		// msg =
-		// "{\"service_name\":\"park_log\",\"data_target\":\"cloud\",\"log_id\":\"100094as_8\",\"content\":\"出场道闸故障收费员手动抬杆lala\",\"operate_time\":1499068894,"
-		//
-	}
-}
